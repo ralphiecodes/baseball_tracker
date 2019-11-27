66ea3850b4ba538f38b1e5aaa834d2ad9020c923
Ralphie Perez <ironware6@gmail.com>
2019-11-27 16:52:28 -0500

    first commit

---
+++ baseball_manager/leads/api.py
@@ -0,0 +1,12 @@
+from leads.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+#Lead View Set
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permissions_classes = [
+        permissions.AllowAny
+    ]
+
+    serializer_class = LeadSerializer
---
+++ baseball_manager/readme.md
@@ -0,0 +1,6 @@
+Requirments
+Python - 
+Django, django_rest_framework, pipenv, psycopg2
+
+Javascript - 
+$ npm i -D webpack webpack-cli
